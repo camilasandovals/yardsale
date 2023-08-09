9d55d48edcd749c832874c3acea2d40e6515b3cb
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-08-09 10:06:42 -0400

    added frontend task

--- DOM/frontendInterview.js
+++ DOM/frontendInterview.js
@@ -3,11 +3,19 @@ import fetch from 'node-fetch'
 const url = 'https://www.algoexpert.io/api/fe/questions'
 
 async function fetchData(url) {
-  const response = await fetch(url)
-  const result = response.json()
-  console.log(result)
-  
+  try {
+    const response = await fetch(url)
+    if(!response.ok) {
+      throw new Error(`error while fetching from ${url}`)
+    }
+    const result = await response.json()
+    return result
+  } catch (error){
+    console.error(error)
+  }
 }
 
 
 fetchData(url)
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
---
+++ FrontendTask/tasks.js
@@ -0,0 +1,92 @@
+let tasks = [];
+// function addTask() {
+  
+//   const taskValue = document.querySelector('input[type="text"]').value
+//   if(!taskValue) return;
+//   task.push(taskValue)
+//   console.log(task)
+//   const li = document.createElement('li')
+  
+//   // checkbox
+//   const checkbox = document.createElement('input')
+//   checkbox.type = 'checkbox'
+
+//   checkbox.addEventListener('change', function(){
+//     if(this.checked) {
+//       this.parentElement.classList.add('completed')
+//     } else {
+//       this.parentElement.classList.remove('completed')
+//     }
+//   })
+
+//   // remove button
+//   const remove = document.createElement('button')
+//   remove.textContent = 'Remove'
+//   remove.addEventListener('click', function() {
+//       this.parentElement.remove()
+//   })
+
+//   // 
+//   const task = document.createTextNode(taskValue)
+
+
+//   li.appendChild(task)
+//   li.appendChild(checkbox)
+//   li.appendChild(remove)
+//   document.querySelector('section ul').append(li)
+//   document.querySelector('input[type="text"]').value = ''
+// }
+
+function addTask() {
+  const taskValue = document.querySelector('input[type="text"]').value
+  if(!taskValue) return
+  
+  tasks.push(taskValue);
+  
+  //clear de input 
+  document.querySelector('input[type=text]').value = ''
+
+  renderTasks()
+}
+
+function renderTasks(){
+  const ul = document.querySelector('section ul')
+
+  ul.innerHTML = ''
+
+  tasks.forEach(taskValue => {
+    const li = document.createElement('li')
+
+    //checkbox
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.addEventListener('change', function() {
+      if(this.checked){
+        this.nextSibling.classList.add('completed')
+      } else {
+        this.nextSibling.classList.remove('completed')
+      }
+    })
+
+    //task 
+    const taskText = document.createElement('span')
+    taskText.textContent = taskValue;
+
+    //Remove
+
+    const remove = document.createElement('button')
+    remove.textContent = 'Remove'
+    remove.addEventListener('click', function() {
+      const index = tasks.indexOf(taskValue)
+      tasks.splice(index, 1);
+      renderTasks();
+    })
+    li.appendChild(checkbox)
+    li.appendChild(taskText)
+    li.appendChild(remove)
+    ul.appendChild(li)
+  })
+
+
+}
+
